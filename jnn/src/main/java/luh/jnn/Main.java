3f3fee22b8a78a1cc827831732a10296038a44b7
Lasse Hueffler <Lasse@hueffler.de>
2024-02-14 19:28:25 +0100

    initial commit

---
+++ jnn/src/main/java/luh/jnn/Main.java
@@ -0,0 +1,11 @@
+package luh.jnn;
+
+import luh.jnn.nn.*;
+
+public class Main {
+  
+  public static void main(String[] args) {
+    System.out.println("Hello Gradle!");
+  }
+}
+
---
+++ jnn/src/main/java/luh/jnn/nn/NeuralNetwork.java
@@ -0,0 +1,5 @@
+package luh.jnn.nn;
+
+public class NeuralNetwork {
+     
+}
