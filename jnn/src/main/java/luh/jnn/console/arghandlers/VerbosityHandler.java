5321058893e9adee91af0f2102928d09484c311c
Lasse Hueffler <Lasse@hueffler.de>
2024-02-18 13:48:53 +0100

    removed unneeded imports from VerbosityHander.java

--- jnn/src/main/java/luh/jnn/console/arghandlers/VerbosityHandler.java
+++ jnn/src/main/java/luh/jnn/console/arghandlers/VerbosityHandler.java
@@ -4,10 +4,8 @@ import luh.jnn.Logging;
 import luh.jnn.console.Arguments;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.Configuration;
-import org.apache.logging.log4j.core.config.Configurator;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 
 /**
